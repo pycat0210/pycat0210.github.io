151c0cfcfd3d7e9e3961ab2bd21cf733175c8185
phos <420853450@qq.com>
2025-02-15 18:47:11 +0800

    修复 BUG

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -9,20 +9,24 @@ import Link from '@/components/Link.vue';
     <Box :style="{height:'100%', display:'flex', flexDirection:'column', alignItems:'center'}">
       <Box :style="{flexGrow:1}"/>
       <Box :style="{display:'flex'}">
-        <Card :style="{display:'flex',  flexDirection:'column', alignItems:'center', gap:'8px'}"> 
+        <Box class="avatar" :style="{display:'flex',  flexDirection:'column', alignItems:'center', gap:'8px'}"> 
           <img src="/favicon.jpg" :style="{width:'128px', height:'128px', borderRadius:'32px'}">
-          <Box> 萌萌磷叶石 </Box>
+          <Box :style="{color:'white'}"> 萌萌磷叶石 </Box>
           <Box>
             <Link href="https://github.com/phos2023" target="_blank">GitHub</Link>
             /
-            <Link href="https://github.com/phos2023/blog" target="_blank">Blog</Link>
+            <Link href="https://phos2023.github.io/blog/" target="_blank">Blog</Link>
           </Box>
-        </Card>
+        </Box>
       </Box>
       <Box :style="{flexGrow:1}"/>
     </Box>
   </main>
 </template>
 
-<style>
+<style scoped>
+.avatar:hover {
+  transform: scale(1.2);
+  transition: all 200ms ease-in-out;
+}
 </style>
